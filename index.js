cbf650c86bbfa369216538a45e94eee2e5f857c3
sheikh-Zulkifal <sheikhzulkifal1@gmail.com>
2024-12-19 23:47:04 +0500

    refactor: remove unused dependencies and add JSON parsing middleware

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const {connectDB} = require("./database/database")
+const { connectDB } = require("./database/database");
 
 const app = express();
 
@@ -13,15 +12,15 @@ connectDB();
 
 app.use(cookieParser());
 
-
 // Root route
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 // add routes
 const userRoutes = require("./routes/userRoutes");
 app.use("/api", userRoutes);
-
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bycrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
